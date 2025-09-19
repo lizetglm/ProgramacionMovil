8d49b3da990f70fc1ad7ea09a12b51be8078a9a1
lizetglm <lizetglm@gmail.com>
2025-09-19 15:15:29 -0600

    Trabajo con listas

---
+++ app/src/main/java/com/example/basicstatecodelab/WellnessTasksList.kt
@@ -0,0 +1,22 @@
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+
+@Composable
+fun WellnessTasksList(
+    modifier: Modifier = Modifier,
+    list: List<WellnessTask> = rememberSaveable { getWellnessTasks() }
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(list) { task ->
+            WellnessTaskItem(taskName = task.label)
+        }
+    }
+}
+
+fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
